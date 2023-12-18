75aa03e49d56ac31eff5902ce78269745b57e06f
Bogdan Brinza <Bogdan.Brinza@hey.com>
2023-12-17 20:02:43 -0800

    Fixing interfaces cleanup

--- key_values_interfaces.go
+++ key_values_interfaces.go
@@ -2,7 +2,15 @@ package kvas
 
 import "io"
 
+type KeyValuesRefresher interface {
+	IndexCurrentModTime() (int64, error)
+	CurrentModTime(key string) (int64, error)
+	IndexRefresh() error
+}
+
 type KeyValues interface {
+	KeyValuesRefresher
+
 	Has(key string) bool
 	Get(key string) (io.ReadCloser, error)
 	GetFromStorage(key string) (io.ReadCloser, error)
@@ -13,8 +21,4 @@ type KeyValues interface {
 	CreatedAfter(timestamp int64) []string
 	ModifiedAfter(timestamp int64, strictlyModified bool) []string
 	IsModifiedAfter(key string, timestamp int64) bool
-
-	IndexCurrentModTime() (int64, error)
-	CurrentModTime(key string) (int64, error)
-	IndexRefresh() error
 }
