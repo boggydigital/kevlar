145b247e666ee14ce07082e3c48b25db588b30b0
Bogdan Brinza <Bogdan.Brinza@hey.com>
2023-12-19 16:45:21 -0800

    Adding ReduxProxy to wrap IdPropertyValues in a real redux object and avoid reimplementing redundant code

---
+++ redux_proxy.go
@@ -0,0 +1,15 @@
+package kvas
+
+func ReduxProxy(idpv map[string]map[string][]string) ReadableRedux {
+	assetKeyValues := make(map[string]map[string][]string)
+	for id, pv := range idpv {
+		for p, v := range pv {
+			if assetKeyValues[p] == nil {
+				assetKeyValues[p] = make(map[string][]string)
+			}
+			assetKeyValues[p][id] = v
+		}
+	}
+
+	return &redux{assetKeyValues: assetKeyValues}
+}
