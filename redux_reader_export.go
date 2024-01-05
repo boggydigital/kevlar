1b1e7f9479a10d04f3d5fd423308759a9cf97287
Bogdan Brinza <Bogdan.Brinza@hey.com>
2024-01-04 17:18:50 -0800

    Fixing export

--- redux_reader_export.go
+++ redux_reader_export.go
@@ -7,18 +7,20 @@ import (
 	"sort"
 )
 
-func (rdx *redux) Export(w io.Writer, assets ...string) error {
+func (rdx *redux) Export(w io.Writer, keys ...string) error {
 
-	if len(assets) == 0 {
-		assets = maps.Keys(rdx.assetKeyValues)
-	}
-
+	assets := maps.Keys(rdx.assetKeyValues)
 	sort.Strings(assets)
 
 	skv := make(wits.SectionKeyValues)
 
-	for _, asset := range assets {
-		skv[asset] = rdx.assetKeyValues[asset]
+	for _, key := range keys {
+		skv[key] = make(wits.KeyValues)
+		for _, asset := range assets {
+			if values := rdx.assetKeyValues[asset][key]; len(values) > 0 {
+				skv[key][asset] = values
+			}
+		}
 	}
 
 	return skv.Write(w)
