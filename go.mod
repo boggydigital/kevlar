cf8eea3571485291d64e230e6c07dd871aff4aeb
Bogdan Brinza <Bogdan.Brinza@hey.com>
2024-07-06 21:35:48 -0700

    Adding a method to migrate from kvas index to kevlar log

--- go.mod
+++ go.mod
@@ -6,5 +6,5 @@ require (
 	github.com/boggydigital/busan v0.1.0
 	github.com/boggydigital/testo v0.1.8
 	github.com/boggydigital/wits v0.2.3
-	golang.org/x/exp v0.0.0-20240506185415-9bf2ced13842
+	golang.org/x/exp v0.0.0-20240613232115-7f521ea00fb8
 )
---
+++ migrate.go
@@ -0,0 +1,105 @@
+package kevlar
+
+import (
+	"encoding/gob"
+	"errors"
+	"github.com/boggydigital/kevlar/kvas_compat"
+	"os"
+	"path/filepath"
+)
+
+// Migrate transforms kvas index to kevlar log and hash files:
+// 0) not part of this method - make sure to create backups before migration!
+// 1) existing index file is decoded to get created, modified dates and hashes
+// 2) new keyValues is connected to the same directory and cast to specific type
+// to get access to internal methods and types (e.g. logRecords)
+// 3) every index record is translated to corresponding log record values
+// 4) hash files are created for each index record with hash
+// 5) log mod time is create with the current timestamp
+// 6) log is written as a single operation (vs kv.appendLogRecord calls)
+// 7) old index is removed to make sure calling migrate again doesn't overwrite new data
+func Migrate(dir, ext string) error {
+
+	// 1)
+
+	asbIndexFilename := filepath.Join(dir, kvas_compat.IndexFilename)
+
+	if _, err := os.Stat(asbIndexFilename); err != nil {
+		return err
+	}
+
+	indexFile, err := os.Open(asbIndexFilename)
+	if err != nil {
+		return err
+	}
+	defer indexFile.Close()
+
+	var index kvas_compat.Index
+
+	if err = gob.NewDecoder(indexFile).Decode(&index); err != nil {
+		return err
+	}
+
+	// 2)
+
+	ikv, err := NewKeyValues(dir, ext)
+	if err != nil {
+		return err
+	}
+
+	kv, ok := ikv.(*keyValues)
+	if !ok {
+		return errors.New("kevlar: unable to cast interface to a specific type")
+	}
+
+	// 3)
+
+	for id, indexRecord := range index {
+
+		kv.log = append(kv.log, &logRecord{
+			Ts: indexRecord.Created,
+			Mt: create,
+			Id: id,
+		})
+
+		if indexRecord.Modified > indexRecord.Created {
+			kv.log = append(kv.log, &logRecord{
+				Ts: indexRecord.Modified,
+				Mt: update,
+				Id: id,
+			})
+		}
+
+		// 4)
+
+		if err = kv.createHashFile(id, indexRecord.Hash); err != nil {
+			return err
+		}
+	}
+
+	// 5)
+
+	if err = kv.createLogMod(); err != nil {
+		return err
+	}
+
+	// 6)
+
+	logRecordsFile, err := os.Create(kv.absLogRecordsFilename())
+	if err != nil {
+		return err
+	}
+	defer logRecordsFile.Close()
+
+	if err = gob.NewEncoder(logRecordsFile).Encode(kv.log); err != nil {
+		return err
+	}
+
+	// 7)
+
+	if err = os.Remove(asbIndexFilename); err != nil {
+		return err
+	}
+
+	return nil
+}
